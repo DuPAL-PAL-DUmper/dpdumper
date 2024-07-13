9647352bdc90e39ad9b6d5c0e2911098e47ef803
Fabio Battaglia <tabaglio@posteo.net>
2024-07-13 09:20:57 +0200

    add some testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,9 @@ import pytest
 @pytest.fixture
 def pin_list_8bit() -> list[int]:
     return [1, 2, 3, 4, 10, 20, 22, 41]
+
+@pytest.fixture
+def pin_list_18bit() -> list[int]:
+    # Taken from a 27C2001, address pins
+    return [12, 11, 10, 9, 8, 7, 6, 5, 27, 26, 23, 25, 4, 28, 29, 3, 2, 30]
+
