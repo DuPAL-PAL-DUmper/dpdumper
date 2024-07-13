01930612ec1cb893c4173b1bc7db75ccf2a4316a
Fabio Battaglia <tabaglio@posteo.net>
2024-07-13 10:38:00 +0200

    Define how the data elements will be returned by a read

--- src/dupico_dumper/hl_board_utilities.py
+++ src/dupico_dumper/hl_board_utilities.py
@@ -1,11 +1,14 @@
 """This module contains high level utility code to perform operations on the board"""
 
-from typing import final, TypeAlias
+from typing import final, NamedTuple
 
 import serial
 from dupico_dumper.ic.ic_definition import ICDefinition
 
-TriStateList: TypeAlias = list[bool | None]
+@final
+class DataElement(NamedTuple):
+    data: int
+    z_mask: int = 0
 
 @final
 class HLBoardUtilities:
@@ -13,6 +16,10 @@ class HLBoardUtilities:
     This class contains high level utilities to perform operations on the board
     """
 
-    @classmethod
-    def check_connection_string(cls, ser: serial.Serial, ic: ICDefinition) -> TriStateList:
-        return [True, None]
+    @staticmethod
+    def read_ic(ser: serial.Serial, ic: ICDefinition) -> list[DataElement]:
+        return []
+    
+    @staticmethod
+    def write_ic(ser: serial.Serial, ic: ICDefinition, data: list[int]) -> bool:
+        return False
